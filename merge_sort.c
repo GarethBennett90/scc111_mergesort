c6b4991f8e66c1203526cfe83042035fc606c572
GarehBennett90 <garethbennett90@gmail.com>
2025-11-10 13:48:54 +0700

    Initialised and printed unsorted array

--- merge_sort.c
+++ merge_sort.c
@@ -7,16 +7,27 @@
 */
 
 
+
+
 int main()
 {
     printf("Merge Sort Implementation");
 
     // Declare and initialise an unsorted array
+    int array[] = {10, 23, 42, 12, 6, 89, 101, 47, 9, 194};
 
     // get the length of the array
+    int size = sizeof(array) / sizeof(array[0]);
 
     // Print the unsorted array
+    printf("The unsorted array is: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
 
+    printf("\n");
+    
     // Recursively break down the array until single elements
 
     // Merge the sub arrays back into the array
