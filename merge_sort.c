6c69062883c2af8e8d155bae74bdcd7ae58fe523
GarehBennett90 <garethbennett90@gmail.com>
2025-11-10 13:36:40 +0700

    Added pseudocode comments and a README

--- merge_sort.c
+++ merge_sort.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+/**
+ * Program to sort a given array
+ * C implementation of the merge sort algorithm
+ * @author - Gareth Bennett
+*/
+
+
 int main()
 {
     printf("Merge Sort Implementation");
+
+    // Declare and initialise an unsorted array
+
+    // get the length of the array
+
+    // Print the unsorted array
+
+    // Recursively break down the array until single elements
+
+    // Merge the sub arrays back into the array
+
+    // Copy the remaining elements if any exist
+    
+    // Print the sorted array result
 
     return 0;
 }
